e2f7e7937902c16d580caff1557513ddb61aaa26
= <kareem.force@gmail.com>
2022-10-31 21:16:10 +0200

    first lec

---
+++ admin/functions/connect.php
@@ -0,0 +1,13 @@
+<?php 
+
+
+define('HOST' , 'localhost');
+define('USER' , 'root');
+define('PASS' , '');
+define('DBNAME' , 'dream_project');
+
+$conn = new mysqli(HOST , USER , PASS , DBNAME);
+
+// arabic
+$conn -> set_charset('utf8');
+
